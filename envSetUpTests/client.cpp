035ba8781617b78c8dc0f3d725cf99d7dd1508a6
Shrinath Korajkar <shrinath.k.korajkar@intimetec.com>
2025-04-27 16:35:56 +0530

    Added env setup test files

---
+++ envSetUpTests/client.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    const char *hello = "Hello from client";
+    char buffer[1024] = {0};
+
+    // Creating socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr << "Connection Failed" << std::endl;
+        return -1;
+    }
+    send(sock, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+    valread = read(sock, buffer, 1024);
+    std::cout << "Message from server: " << buffer << std::endl;
+
+    close(sock);
+    return 0;
+}
